5a3c2d9f67dd764af8cfc1396935191cdfab5e98
Syed Umair Ali <umairnaqvi902@gmail.com>
2025-11-04 17:32:34 +0500

    update the description

--- app/layout.js
+++ app/layout.js
@@ -15,9 +15,49 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// ✅ Metadata
 export const metadata = {
-  title: "Syed Umair",
-  description: "for seo",
+  title: "Syed Umair | Frontend Developer Portfolio",
+  description:
+    "Hi, I'm Umair Naqvi, a passionate web developer with expertise in Next.js and React. With years of experience building scalable, high-performance web applications, I specialize in creating responsive and user-friendly interfaces. Whether you're looking to develop a complex app or a dynamic website, I can help bring your vision to life with cutting-edge technologies and best practices.",
+  keywords: [
+    "Syed Umair",
+    "Frontend Developer",
+    "React Developer",
+    "Next.js Developer",
+    "Web Developer Portfolio",
+    "Naqvi",
+    "portfolio",
+    "developer",
+    "nextjs",
+    "Syed Umair",
+    "umair",
+  ],
+  authors: [{ name: "Syed Umair" }],
+  openGraph: {
+    title: "Syed Umair | Frontend Developer Portfolio",
+    description:
+      "Hi, I'm Syed Umair, a passionate web developer with expertise in Next.js and React.",
+    url: "https://syedumair.vercel.app",
+    siteName: "Syed Umair Portfolio",
+    images: [
+      {
+        url: "https://syedumair.vercel.app/logo.png", // ✅ full absolute URL!
+        width: 1200,
+        height: 630,
+        alt: "Syed Umair Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Syed Umair | Frontend Developer Portfolio",
+    description:
+      "Hi, Syed Umair, a passionate web developer with expertise in Next.js and React.",
+    images: ["https://syedumair.vercel.app/logo.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
